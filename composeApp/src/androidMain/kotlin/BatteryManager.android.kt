77d031677be928eccb9d66bab7a3e63d3f4dacac
Rakib Rz  <zudarakib@gmail.com>
2024-08-08 22:14:32 +0530

    Initial commit

---
+++ composeApp/src/androidMain/kotlin/BatteryManager.android.kt
@@ -0,0 +1,16 @@
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import kotlin.math.roundToInt
+
+actual class BatteryManager(private var context: Context) {
+
+    actual fun getBatteryLevel(): Int {
+        val intentFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        val batterStatus = context.registerReceiver(null, intentFilter)
+        val level = batterStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
+        val scale = batterStatus?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
+        return (level / scale.toFloat() * 100).roundToInt()
+    }
+}
