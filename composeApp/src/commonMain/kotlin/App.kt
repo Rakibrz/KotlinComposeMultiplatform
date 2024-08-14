93a23cf7f07ab789f2b7ca47b8164091e8db6952
Rakib Rz  <zudarakib@gmail.com>
2024-08-14 19:02:51 +0530

    Greetings

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,20 +1,12 @@
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
-fun App(batteryManager: BatteryManager) {
+fun App() {
     MaterialTheme {
-        Box(modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-            ) {
-            Text(text = "Battery Level is ${batteryManager.getBatteryLevel()}")
-        }
+        Text(text = Greeting().greet())
     }
 }
